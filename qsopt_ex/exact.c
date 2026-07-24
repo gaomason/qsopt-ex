2c2b137c0c2f885a0217536dc6b48659aaba8cd8
gaomason <mason2022052@gmail.com>
2026-07-24 20:14:19 +0800

    fixed error hopefully

--- qsopt_ex/exact.c
+++ qsopt_ex/exact.c
@@ -1558,8 +1558,7 @@ static int QSexact_basis_status (mpq_QSdata * p_mpq,
 			int best_entering_col = -1;
 			int min_spike_sparsity = p_mpq->lp->nrows + 1;
 			int max_nnz = -1;
-			
-			char *reach_workspace = (char *) ILL_UTIL_SAFE_MALLOC (p_mpq->lp->nrows, char, reach_workspace);
+			reach_workspace = (char *) ILL_UTIL_SAFE_MALLOC (p_mpq->lp->nrows, char, reach_workspace);
 			if (reach_workspace == NULL) {
 				mpf_ILLsvector_free (&rhs);
 				mpf_ILLsvector_free (&v);
